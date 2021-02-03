e911767986e4f8b05105670b0b3ff1016c0599ef
Riku Takanashi <highpear.0619@gmail.com>
2021-02-04 01:56:25 +0900

    フォルダ選択画面を追加

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
+from tkinter import *
+from tkinter import ttk
 
+def open_dir():
+    print('フォルダを開きます...')
 
 
 if __name__ == '__main__':
-    print('Hello')
+    
+    # rootの初期化
+    root = Tk()
+    root.title('Photo Neat')
+    root.geometry('800x400')
+
+    main_frame = ttk.Frame(root, padding=4)
+    
+    path_entry = ttk.Entry(main_frame, width=30)
+    path_entry.insert(END, 'フォルダを開くかパスを指定して下さい')
+    
+    open_dir_button = ttk.Button(main_frame, text='フォルダを開く', command=open_dir)
+
+    files_label = ttk.Scrollbar
+
+    info_label = ttk.Label(main_frame, text='情報:\nフォルダが選択されていません')
+    
+
+    # ウィジェットの色
+    root.configure(background='gray')
+
+
+    # ウィジェットの配置
+    main_frame.pack()
+    path_entry.pack(side=LEFT)
+    open_dir_button.pack(side=LEFT)
+    info_label.pack(side=BOTTOM)
+
+
+    root.mainloop()
