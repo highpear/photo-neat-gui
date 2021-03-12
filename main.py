881e3cd426c44bf0b7f929faf17ac5742bdf9654
Riku Takanashi <highpear.0619@gmail.com>
2021-03-13 00:21:47 +0900

    選択したフォルダ内の一覧表示

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+# tkinter
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
 
+# other imports
+import os
+
 current_dir = './'
+flist = []
+
 
 # フォルダを指定して開く (デフォルトはカレント)
 def open_dir():
     print('フォルダを開きます...')
     open_dir = filedialog.askdirectory(initialdir=current_dir)
+    update_flist(open_dir)
     return open_dir  # 開いたフォルダのパスを返す
+
+
+# 画面に表示されているファイルを更新
+def update_flist(dir):
+    for f in os.listdir(dir):
+        flist.append(f)
+        lbox.insert(END, f)
+        print(f, 'was added')
+    info_label['text'] = '場所:{}'.format(dir)
 
 
 if __name__ == '__main__':
@@ -29,6 +45,7 @@ if __name__ == '__main__':
 
     info_label = ttk.Label(main_frame, text='情報:\nフォルダが選択されていません')
 
+    lbox = Listbox(main_frame, listvariable=StringVar(value=flist), height=20, selectmode=MULTIPLE)
 
 
     
@@ -42,6 +59,7 @@ if __name__ == '__main__':
     path_entry.pack(side=LEFT)
     open_dir_button.pack(side=LEFT)
     info_label.pack(side=BOTTOM)
+    lbox.pack(side=TOP)
 
 
     root.mainloop()
